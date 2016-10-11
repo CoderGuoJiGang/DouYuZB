c02b3001a0c07d9be4abc3cca26ce615a892e6fa
student <guojigang@guojigangdemacbook-pro.local>
2016-10-11 17:44:29 +0800

    添加三方库等

---
+++ DYZB/DYZB/Classes/Tools/NetWorkTools.swift
@@ -0,0 +1,24 @@
+//
+//  NetWorkTools.swift
+//  DYZB
+//
+//  Created by 郭吉刚 on 16/9/29.
+//  Copyright © 2016年 郭吉刚. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+enum MethodType {
+    case GET
+    case POST
+}
+class NetWorkTools {
+    
+    class func requestData(type : MethodType,urlString:String,params:[String:String]?=nil,finishedCallBack: (_ result : AnyObject) -> ()){
+        
+        
+        
+    }
+    
+   }
